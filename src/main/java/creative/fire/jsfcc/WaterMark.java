b71dbadd1dda8e1e7eb2f865ec7982fb6992f874
Eric Han <feuyeux@gmail.com>
2012-03-27 22:00:31 +0800

    auto

--- src/main/java/creative/fire/jsfcc/WaterMark.java
+++ src/main/java/creative/fire/jsfcc/WaterMark.java
@@ -12,7 +12,6 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Serializable;
-
 import javax.imageio.ImageIO;
 
 class WaterMark implements Serializable {
@@ -25,26 +24,18 @@ class WaterMark implements Serializable {
 
 		try {
 			Image srcImg = ImageIO.read(new File(image));
-
 			BufferedImage buffImg = new BufferedImage(srcImg.getWidth(null), srcImg.getHeight(null), BufferedImage.TYPE_INT_RGB);
-
 			int width = buffImg.getWidth();
 			int height = buffImg.getHeight();
-
 			// 得到画笔对象
 			// Graphics g= buffImg.getGraphics();
 			Graphics2D g = buffImg.createGraphics();
-
 			// 设置对线段的锯齿状边缘处理
 			g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-
 			g.drawImage(srcImg.getScaledInstance(srcImg.getWidth(null), srcImg.getHeight(null), Image.SCALE_SMOOTH), 0, 0, null);
-
 			g.rotate(Math.toRadians(rotate), (double) width / 2, (double) height / 2);
-
 			// 设置颜色
 			g.setColor(color);
-
 			// 设置 Font
 			g.setFont(new Font("微软雅黑", Font.BOLD, 18));
 
@@ -59,16 +50,11 @@ class WaterMark implements Serializable {
 				xoffset = width - len;
 				yoffset = height - 30;
 			}
-
 			g.drawString(text, xoffset, yoffset);
-
 			g.dispose();
-
 			os = new FileOutputStream(textImage);
-
 			// 生成图片
 			ImageIO.write(buffImg, "PNG", os);
-
 			String result = textImage.substring(textImage.lastIndexOf("/") + 1, textImage.length());
 			return result;
 		} catch (Exception e) {
