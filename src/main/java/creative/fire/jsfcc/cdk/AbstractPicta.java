7f141032cd6792955d9dcc315276b7f94dc9bd92
Eric Han <feuyeux@gmail.com>
2012-04-03 03:40:36 +0800

    auto

--- src/main/java/creative/fire/jsfcc/cdk/AbstractPicta.java
+++ src/main/java/creative/fire/jsfcc/cdk/AbstractPicta.java
@@ -1,10 +1,6 @@
 package creative.fire.jsfcc.cdk;
 
-import javax.faces.event.ActionEvent;
-import javax.faces.event.ActionListener;
-
 import org.richfaces.cdk.annotations.Attribute;
-import org.richfaces.cdk.annotations.Event;
 import org.richfaces.cdk.annotations.JsfComponent;
 import org.richfaces.cdk.annotations.JsfRenderer;
 import org.richfaces.cdk.annotations.Tag;
@@ -13,21 +9,14 @@ import org.richfaces.cdk.annotations.Tag;
  * @version 1.0
  */
 
-@JsfComponent(type = "creative.fire.jsfcc.cdk.picta", family = "creative.fire.picta", generate = "creative.fire.jsfcc.cdk.component.UIBasicpicta", 
-renderer = @JsfRenderer(type = "creative.fire.picta"), 
-tag = @Tag(name = "picta"), 
-fires = @Event(value = ActionEvent.class, listener = ActionListener.class, 
-listenerMethod = "switching",
-tag=@Tag(name="switching",generate=true)))
+@JsfComponent(
+		type = "creative.fire.jsfcc.cdk.picta", 
+		family = "creative.fire.picta", 
+		generate = "creative.fire.jsfcc.cdk.component.UIBasicpicta", 
+		renderer = @JsfRenderer(type = "creative.fire.picta"), 
+		tag = @Tag(name = "picta")
+)
 public abstract class AbstractPicta extends javax.faces.component.UIOutput {
 	@Attribute(defaultValue = "0.png")
 	public abstract String getImage();
-
-	public void fillFacesListener(ActionListener listener) {
-		addFacesListener(listener);
-	}
-
-	public void dropFacesListener(ActionListener listener) {
-		removeFacesListener(listener);
-	}
 }
